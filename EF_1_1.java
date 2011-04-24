5ff93b743743010a9f96bd0d9e9e0555e7fec22d
Ivan C <ivano666@gmail.com>
2011-04-23 19:57:38 -0700

    added Transducer and extra input and output ports

--- EF_1_1.java
+++ EF_1_1.java
@@ -25,22 +25,30 @@ public class EF_1_1 extends ViewableDigraph{
 
 // Structure information start
         // Add input port names
+        addInport("stats");
+        addInport("ErrorFile");
 
         // Add output port names
         addOutport("start");
         addOutport("FFout");
+        addOutport("measures");
 
 //add test input ports:
 
         // Initialize sub-components
-        ViewableAtomic OperationalSystem_1_0 =  new OperationalSystem_0_0("OperationalSystem_1_0");
+        ViewableAtomic operationalSystem_1_0 =  new OperationalSystem_0_0("OperationalSystem_1_0");
+        ViewableAtomic transducer_1_0 = new Transducer_0_0("Transducer_1_0");
 
         // Add sub-components
-        add(OperationalSystem_1_0);
+        add(operationalSystem_1_0);
+        add(transducer_1_0);
 
         // Add Couplings
-        addCoupling(this, "FFout", OperationalSystem_1_0, "FFout");
-        addCoupling(this, "start", OperationalSystem_1_0, "start");
+        addCoupling(this, "FFout", operationalSystem_1_0, "FFout");
+        addCoupling(this, "start", operationalSystem_1_0, "start");
+        addCoupling(this, "stats", transducer_1_0, "stats");
+        addCoupling(this, "ErrorFile", transducer_1_0, "ErrorFile");
+        addCoupling(this, "measures", transducer_1_0, "measures");
 
 // Structure information end
         initialize();
