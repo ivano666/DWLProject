f6ceca4293ad321cb0ab2a87131763b287cb6100
ivano666 <ivano666@gmail.com>
2011-04-29 16:45:10 -0700

    reworkd couplings due to changes in DW_1_1

--- DWLProject_0_1.java
+++ DWLProject_0_1.java
@@ -12,6 +12,7 @@ package DWLProject;
 import java.awt.Dimension;
 import java.awt.Point;
 
+import view.modeling.ViewableAtomic;
 import view.modeling.ViewableComponent;
 import view.modeling.ViewableDigraph;
 
@@ -36,7 +37,7 @@ public class DWLProject_0_1 extends ViewableDigraph{
         // Initialize sub-components
         ViewableDigraph EF_1_1 =  new EF_1_1("EF_1_1");
         ViewableDigraph DWL_1_1 =  new DWL_1_1("DWL_1_1");
-        ViewableDigraph DW_1_1 =  new DW_1_1("DW_1_1");
+        ViewableAtomic DW_1_1 =  new DW_1_1("DW_1_1");
 
         // Add sub-components
         add(EF_1_1);
@@ -44,9 +45,11 @@ public class DWLProject_0_1 extends ViewableDigraph{
         add(DW_1_1);
 
         // Add Couplings
-        addCoupling(DWL_1_1, "insert", DW_1_1, "insert");
+        addCoupling(DWL_1_1, "load", DW_1_1, "load");
         addCoupling(EF_1_1, "FFout", DWL_1_1, "FFin");
         addCoupling(EF_1_1, "start", DWL_1_1, "start");
+        addCoupling(DW_1_1, "stats", EF_1_1, "stats");
+        addCoupling(DWL_1_1, "ErrorFile", EF_1_1, "ErrorFile");
 
 // Structure information end
         initialize();
@@ -59,8 +62,8 @@ public class DWLProject_0_1 extends ViewableDigraph{
     public void layoutForSimView()
     {
         preferredSize = new Dimension(1618, 560);
-        ((ViewableComponent)withName("EF_1_1")).setPreferredLocation(new Point(20, 46));
         ((ViewableComponent)withName("DWL_1_1")).setPreferredLocation(new Point(419, 44));
-        ((ViewableComponent)withName("DW_1_1")).setPreferredLocation(new Point(1139, 40));
+        ((ViewableComponent)withName("DW_1_1")).setPreferredLocation(new Point(1180, 244));
+        ((ViewableComponent)withName("EF_1_1")).setPreferredLocation(new Point(20, 46));
     }
     }
